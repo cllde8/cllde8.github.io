ca9e4444e3c0e52e006167b0ccfc6831c19a5e54
chia-lin.lin <chia-lin.lin@kit.edu>
2024-01-16 16:22:26 +0100

    Added dataset and index

--- script.sh
+++ script.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 directoryPath="./labimotion/schema"
-baseUrl="https://labimotion.github.io/schemas/v0.0.1"
+# baseUrl="https://labimotion.github.io/schema/v1.0"
+baseUrl="https://cllde8.github.io/labimotion/schema/v1.0"
 
-for file in $directoryPath/*.json
+for file in $(find $directoryPath -name "*.json")
 do
   if jq -e '."$id"' $file > /dev/null; then
     filename=$(basename -- "$file") # gets the filename from the file path
-    filename="${filename#sch-}" # removes the sch- prefix
+    # filename="${filename#sch-}" # removes the sch- prefix
     newId="$baseUrl/$filename"
     jq --arg newId "$newId" '."$id" |= $newId' $file > temp.json && mv temp.json $file
     echo "Updated \$id in $file to $newId"
